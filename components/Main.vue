c8a5ecee6d9b9f46f152a1e1740c90419cba4d7b
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-07-20 22:24:51 +0200

    added PartnersLogo component

--- components/Main.vue
+++ components/Main.vue
@@ -1,6 +1,24 @@
 <template>
-  <ListComponent :items="myItems"> </ListComponent>
-  <Btn />
+  <div class="main-top">
+    <div class="main-left">
+      <h2>Rýchle výsledky pomocou UX auditu.</h2>
+
+      <p>
+        UX auditom Vášho riešenia lepšie pochopíte, ako vaší zákazníci používajú
+        Váš produkt, zvýšite ich spokojnosť a hlavne zisky.
+      </p>
+
+      <p>
+        Dosiahnite lepšie výsledky vašej webstránky alebo e-shopu pomocou UX
+        auditu.
+      </p>
+    </div>
+    <div class="main-right">
+      <PartnersLogo />
+      <ListComponent :items="myItems"> </ListComponent>
+      <Btn />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -19,3 +37,14 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.main-top {
+  display: flex;
+}
+.main-left {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
---
+++ components/PartnersLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="logo-partners">
+    <img src="/img/logo1.png" alt="Logo 1" class="partners-logo" />
+    <img src="/img/logo2.png" alt="Logo 2" class="partners-logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PartnersLogo",
+};
+</script>
+
+<style lang="scss">
+.logo-partners {
+  width: 100%;
+
+  img {
+    width: 180px;
+  }
+}
+</style>
