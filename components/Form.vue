aaf78c039578f81bc22a31a0738200801bb5afd3
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-07-28 09:58:09 +0200

    adjusted form css

--- components/Form.vue
+++ components/Form.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="backdrop">
-    <form @submit.prevent="onSubmit" class="form">
-      <h2 class="form-headline">Jednoducho nám napíšte a my sa Vám ozveme.</h2>
-      <span class="close-icon"
-        ><MaterialSymbolsLightCancel @click="closeModal">
-        </MaterialSymbolsLightCancel
-      ></span>
-      <Input>Meno:</Input>
-      <div class="number">
-        <Input>Email:</Input>
-        <Input>Tel. čislo:</Input>
-      </div>
-      <Input>Webstránka:</Input>
-      <Textarea>Poznámka:</Textarea>
-      <Btn width="90%" height="70px" fontSize="1.1rem" class="contact-btn"
-        >Kontaktujte ma.</Btn
-      >
-      <!-- <button class="contact-btn">Kontaktujte ma.</button> -->
-    </form>
+  <div class="modal">
+    <div class="modal-content">
+      <form @submit.prevent="onSubmit" class="form">
+        <h2 class="form-headline">
+          Jednoducho nám napíšte a my sa Vám ozveme.
+        </h2>
+        <span class="close-icon"
+          ><MaterialSymbolsLightCancel @click="closeModal">
+          </MaterialSymbolsLightCancel
+        ></span>
+        <Input>Meno:</Input>
+        <div class="number">
+          <Input>Email:</Input>
+          <Input>Tel. čislo:</Input>
+        </div>
+        <Input>Webstránka:</Input>
+        <Textarea>Poznámka:</Textarea>
+        <Btn width="90%" height="70px" fontSize="1.1rem" class="contact-btn"
+          >Kontaktujte ma.</Btn
+        >
+        <!-- <button class="contact-btn">Kontaktujte ma.</button> -->
+      </form>
+    </div>
   </div>
 </template>
 
@@ -36,20 +40,30 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap");
 
 $base-font: "Ubuntu", sans-serif;
-.form {
-  position: relative;
-  padding: 1em;
-  width: 600px;
-  margin: auto;
-  background-color: white;
-  box-shadow: 0 0 30px 5px rgba(0, 0, 0, 0.1);
-}
-.backdrop {
+
+.modal {
   top: 0;
   position: fixed;
   background: rgba(255, 255, 255, 0.9);
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start; /* Align to the top of the screen */
+  z-index: 1000;
+  overflow-y: auto; /* Allows the modal to scroll if content is too large */
+}
+
+.modal-content {
+  position: relative;
+  padding: 1em;
+  // width: 600px;
+  margin: auto;
+  background-color: white;
+  box-shadow: 0 0 30px 5px rgba(0, 0, 0, 0.1);
+  max-width: 500px;
+  width: 90%;
+  margin-top: 100px; /* Set the desired margin-top */
 }
 
 .close-icon {
--- components/Input.vue
+++ components/Input.vue
@@ -11,6 +11,7 @@
 @import url("https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap");
 
 $base-font: "Ubuntu", sans-serif;
+
 .form-input {
   font-family: $base-font;
   width: 90%;
