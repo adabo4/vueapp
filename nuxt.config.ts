a0e60de544b5f71a1c16f143383bf69f6313ac71
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-07-29 17:23:18 +0200

    added global scss file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   pages: true,
+  css: [
+    "@/assets/styles/global.scss", // Add the path to your global SCSS file here
+  ],
+  build: {
+    loaders: {
+      scss: {
+        additionalData: '@import "@/assets/styles/global.scss";', // Ensure the correct path
+      },
+    },
+  },
 });
