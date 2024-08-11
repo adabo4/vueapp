5300757a2179fc7b470da9ada2def448feee306a
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-08-11 21:27:04 +0200

    deleted unused styles from Input and Headline

--- components/Headline.vue
+++ components/Headline.vue
@@ -5,16 +5,3 @@
 </template>
 
 <script setup></script>
-
-<style lang="scss">
-// @import url("https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap");
-
-// $base-font: "Ubuntu", sans-serif;
-// .header-headline {
-//   color: white;
-//   font-family: $base-font;
-//   font-size: 3rem;
-//   text-align: center;
-//   line-height: 1.6em;
-// }
-</style>
