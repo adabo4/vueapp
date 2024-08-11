8c45e7cbfac7ef52e2d067ef550208ac212431ed
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-08-11 21:54:21 +0200

    changed ListComponent into composition API

--- components/ListComponent.vue
+++ components/ListComponent.vue
@@ -1,22 +1,20 @@
 <template>
   <ul>
-    <li v-for="(item, index) in items" :key="index">
+    <li v-for="(item, index) in props.items" :key="index">
       <MaterialSymbolsCheckCircle class="check-icon" />
       {{ item }}
     </li>
   </ul>
 </template>
 
-<script>
+<script setup lang="ts">
 import MaterialSymbolsCheckCircle from "~/components/icons/MaterialSymbolsCheckCircle.vue";
+import { defineProps } from "vue";
 
-export default {
-  name: "ListComponent",
-  props: {
-    items: {
-      type: Array,
-      required: true,
-    },
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
   },
-};
+});
 </script>
