97be0c91542afe594eeee77cde307d9432ccdd0b
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-08-11 20:19:18 +0200

    deleted unused styles from Logo, ListComponent

--- components/ListComponent.vue
+++ components/ListComponent.vue
@@ -20,34 +20,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-// @import url("https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap");
-
-// $base-font: "Ubuntu", sans-serif;
-// * {
-//   margin: 0;
-//   padding: 0;
-//   box-sizing: border-box;
-// }
-// ul {
-//   list-style-type: none;
-//   line-height: 2.5rem;
-// }
-
-// li {
-//   padding: 5px;
-//   font-family: $base-font;
-//   font-size: 1.2em;
-//   display: flex;
-//   gap: 0.3em;
-//   align-items: center;
-// }
-
-// .check-icon {
-//   color: #0bac0b;
-//   width: 1.5em; /* Set a fixed width */
-//   height: 1.5em; /* Set a fixed height */
-//   flex-shrink: 0; /* Prevent shrinking */
-// }
-</style>
