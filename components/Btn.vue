712060ef36fce8b29a2082b1195f28e2be66a96c
adabo4 <130857407+adabo4@users.noreply.github.com>
2024-08-11 20:16:11 +0200

    deleted unused code from Btn.vue

--- components/Btn.vue
+++ components/Btn.vue
@@ -8,10 +8,6 @@
 export default {
   name: "Btn",
   props: {
-    // width: {
-    //   type: String,
-    //   default: "auto",
-    // },
     height: {
       type: String,
       default: "auto",
@@ -38,7 +34,6 @@ export default {
     },
     customStyle() {
       return {
-        // width: this.width,
         height: this.height,
         fontSize: this.fontSize,
         padding: this.padding,
@@ -47,18 +42,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap");
-
-$base-font: "Ubuntu", sans-serif;
-// .btn {
-//   background-color: #eb345c;
-//   font-weight: 600;
-//   color: white;
-//   border-radius: 0.5rem;
-//   border: none;
-//   font-family: $base-font;
-//   width: 100%;
-// }
-</style>
